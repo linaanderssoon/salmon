a019e126dc170fe413b4db60b34cf789ec00f399
eelsalindh <elsalindh@gmail.com>
2021-05-10 15:42:38 +0200

    Destinationer sida bad + filändringar
    Lagt js filer i mappar och flyttat bilder

--- studyAbroad/JSfiler/destinations.js
+++ studyAbroad/JSfiler/destinations.js
@@ -9,46 +9,111 @@
 // - Denna funktionen kallas på onChange i selectorerna.
 // - Skapar divvar med destinationerna och lägger in dem i wrapper/föräldern
 
-createDestinations();
 
+//SKAPAR WRAPPER FÖR DESTINATIONER OCH DESS INNEHÅLL
 function goToDestinations(country) {
+    
     wrapper.innerHTML = '';
 
     wrapper.innerHTML = `
     <div class="destHeader">
         <h1> Destinationer </h1>
-        <p>Här kan du söka bland alla tillgänliga destinationer.</p>
+        <p> Här kan du söka bland alla tillgänliga destinationer.</p>
     </div>
     <div class="selectorsDiv"></div>
 
     <div class="longAd"></div>
 
     <div class="citiesWrapper">
-        <div class="citiesParent">
-            <div class="cityImage"></div>
-            <h2 class="cityText"></h2>
-        </div>
+        
     </div>
 
-    <div class="showMore"></div>
+    <div class="showMore">
+        <p> Visa mer </p>
+        <img class="showMoreImg" src="CSSfiler/ikoner/arrowDown.png">
+    </div>
 
     <div class="longAd"></div>
 
     `;
-}
 
-const citiesWrapper = document.querySelector('.citiesWrapper');
+    // let destHeader = document.createElement('div');
+    // destHeader.classList.add('destHeader');
+    // destHeader.innerHTML = `
+    //     <h1> Destinationer </h1>
+    //     <p> Här kan du söka bland alla tillgänliga destinationer.</p>
+    // `
 
-function createDestinations(country) {
+    // let selectorsDiv = document.createElement('div');
+    // selectorsDiv.classList.add('selectorsDiv');
+
+    // let longAd = document.createElement('div');
+    // longAd.classList.add('longAd');
+
+    // let citiesWrapper = document.createElement('div');
+    // citiesWrapper.classList.add('citiesWrapper');
+
+    // let showMore = document.createElement('div');
+    // showMore.classList.add('showMore');
+
+    // let longAd2 = document.createElement('div');
+    // longAd2.classList.add('longAd');
+
+    // wrapper.append(destHeader, selectorsDiv, longAd, citiesWrapper, showMore, longAd2)
+
+    let selectors = document.querySelector('.selectorsDiv');
+
+    selectors.innerHTML = `
+        <div class="dropdown">
+            <select name="" id="country">
+                <option id="selectItem" value="">All countries</option>
+            </select>
+            <div id="arrow"></div>
+        </div>
+    `
+
+    //SELECTION FIELD
+    const selectCountries = document.getElementById('country');
+
+    COUNTRIES.forEach(c => {
+        let newOption = document.createElement('option');
+        newOption.textContent = c.name;
+        newOption.classList.add('selectItem');
+        selectCountries.append(newOption);
+    });
+
+    let citiesWrapper = document.querySelector('.citiesWrapper');
+
+    selectCountries.addEventListener('onChange', createDestinations);
+
+
+    //CREATE CITY DIVS
+    function createDestinations(country) {
+
+        
         // skapar rutorna med destinationer
         // - Denna funktionen kallas på onChange i selectorerna.
         // - Skapar divvar med destinationerna och lägger in dem i wrapper/föräldern
-    let citiesParent = document.createElement('div');
-    citiesParent.classList.add('.citiesParent');
-    citiesWrapper.append(citiesParent);
+        for(let i = 0; i < 9; i++) {
+            let citiesParent = document.createElement('div');
+            citiesParent.classList.add('citiesParent');
+            citiesWrapper.append(citiesParent);
+    
+            citiesParent.innerHTML = `
+                <img class="cityImage" src="CSSfiler/ikoner/logotyp.png">
+                <h2 class="cityText"> ${COUNTRIES.name} </h2>   
+            `;
+        }
+
+    
 
 
+
+    }
+
+    // createDestinations();
 
 }
+
 
 
