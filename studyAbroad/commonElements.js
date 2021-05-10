ce623b2b876e71e31743dede9a44f6975ba1bf0c
linaanderssoon <82369814+linaanderssoon@users.noreply.github.com>
2021-05-10 10:05:28 +0200

    lagt in i mapparna

--- studyAbroad/commonElements.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const wrapper = document.getElementById('wrapper');
-const navHome = document.getElementById('home');
-const navDestinations = document.getElementById('destinations');
-const navProgrammes = document.getElementById('programmes');
-const navInterviews = document.getElementById('interviews');
