1313466394a11d531fcb5584b0b3bc7b2662bfcc
Pranvera97 <pranveragerbeshi@gmail.com>
2021-05-11 14:27:02 +0200

    Karusellen färdig
    Färdig Karusell

--- studyAbroad/JSfiler/home.js
+++ studyAbroad/JSfiler/home.js
@@ -1,28 +1,32 @@
 "use strict";
 
-navHome.classList.add('currentPage');
-
 goToHome();
 
+navHome.classList.add('currentPage');
+
+
+
 function goToHome(){
-    navHome.classList.add('currentPage');
 
-    navInterviews.classList.remove('currentPage');
-    navDestinations.classList.remove('currentPage');
-    navProgrammes.classList.remove('currentPage');
+  wrapper.innerHTML = '';
 
-    wrapper.innerHTML = '';
+  navHome.classList.add('currentPage');
 
-    wrapper.innerHTML = `
+  navInterviews.classList.remove('currentPage');
+  navDestinations.classList.remove('currentPage');
+  navProgrammes.classList.remove('currentPage');
+
+  
+  wrapper.innerHTML = `
     <div class="homeHeader">
-            <div class="homeHeaderRight">
-             <div class="homeHeaderImg"></div>
+          <div class="homeHeaderRight">
+          <div class="homeHeaderImg"></div>
         </div>
         <div class="homeHeaderLeft">
-             <h1>Är du redo att 
-                 <br> upptäcka världen?</h1>
+            <h1>Är du redo att 
+               <br> upptäcka världen?</h1>
              <p>Här hittar du allt du behöver 
-                 <br> veta inför dina utlanddstudier</p>
+               <br> veta inför dina utlanddstudier</p>
              <button>Destinationer</button>
              <button>Program</button>
         </div>
@@ -38,14 +42,13 @@ function goToHome(){
         <div class="carousel-container">
         <div class="carousel-inner">
           <div class="track">
-      
           </div>
         </div>
         <div class="arrows">
-          <div class="prev"  id="arrowPrev" class="arrowCircle">
+          <div class="prev"  id="arrowPrev">
               <div id="arrowImg1"></div>
           </div>
-          <div class="next" id="arrowNext" class="arrowCircle">
+          <div class="next" id="arrowNext">
               <div id="arrowImg2"></div>
           </div>
         </div>
@@ -85,5 +88,65 @@ function goToHome(){
 
 <div class="longAd"></div>
     `;
+
+
+    let carouselTrack = document.querySelector('.track');
+    
+    for(let i = 0; i < COUNTRIES.length; i++){
+      let carouselContainer = document.createElement("div");
+      carouselContainer.classList.add("card-container");
+  
+      carouselContainer.innerHTML=`
+      <div class="card">
+      <img class="carouselImg" src="JSfiler/Images/${COUNTRIES[i].imagesNormal[1]}">
+      <h2 class="carouselInfo"> ${COUNTRIES[i].name}</h2> 
+      </div>
+      `;
+  
+      carouselTrack.append(carouselContainer);
+    }
+
+
+
+let prev  = document.querySelector('.prev');
+let next = document.querySelector('.next');
+
+let track = document.querySelector('.track');
+
+let carouselWidth = document.querySelector('.carousel-container').offsetWidth;
+
+window.addEventListener('resize', () => {
+  carouselWidth = document.querySelector('.carousel-container').offsetWidth;
+})
+
+let index = 0;
+
+next.addEventListener('click', () => {
+  index++;
+  prev.classList.add('show');
+  track.style.transform = `translateX(-${index * carouselWidth}px)`;
+  
+  if (track.offsetWidth - (index * carouselWidth) < carouselWidth) {
+    next.classList.add('hide');
+  }
+})
+
+prev.addEventListener('click', () => {
+  index--;
+  next.classList.remove('hide');
+  if (index === 0) {
+    prev.classList.remove('show');
+  }
+  track.style.transform = `translateX(-${index * carouselWidth}px)`;
+});
+   
 }
+
+
+
+
+
+
+
+
 
